96bb1bb483a71df60a289ba1ceed6ed49476027c
sourav11705 <singhkunalsingh814@gmail.com>
2026-04-25 05:27:53 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -23,7 +23,7 @@ app.add_middleware(
     allow_origins=[
         "https://sportsmark.web.app",
         "*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
